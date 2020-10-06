a0fbef994a32c9da403a1f1f487fc40944192862
SHANKAR DAKOLIA <itglobetech2020@gmail.com>
2020-10-06 15:50:07 +0530

    Notes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,7 +11,8 @@ const routes: Routes = [
   { path: 'customers', loadChildren: () => import('./customers/customers.module').then(m => m.CustomersModule) },
   { path: 'employees', loadChildren: () => import('./employees/employees.module').then(m => m.EmployeesModule) },
   { path: 'stocks', loadChildren: () => import('./stocks/stocks.module').then(m => m.StocksModule) },
-  { path: 'githubSearch', loadChildren: () => import('./github-search/github-search.module').then(m => m.GithubSearchModule) }
+  { path: 'githubSearch', loadChildren: () => import('./github-search/github-search.module').then(m => m.GithubSearchModule) },
+  { path: 'examples', loadChildren: () => import('./examples/examples.module').then(m => m.ExamplesModule) }
 ];
 
 @NgModule({
