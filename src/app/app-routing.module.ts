54c7fe038749a753bd35cee762b14fe9ce0c4467
SHANKAR DAKOLIA <itglobetech2020@gmail.com>
2020-10-06 15:28:47 +0530

    First Upload

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AboutComponent } from './layouts/about/about.component';
+import { ContactComponent } from './layouts/contact/contact.component';
+import { HomeComponent } from './layouts/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:HomeComponent},
+  {path:'about', component:AboutComponent},
+  {path:'contact', component:ContactComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './layouts/home/home.component';
+import { NavbarComponent } from './layouts/navbar/navbar.component';
+import { AboutComponent } from './layouts/about/about.component';
+import { ContactComponent } from './layouts/contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    AboutComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
