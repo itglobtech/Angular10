fd9cf6b1f7e51f6f40eed1d49d2ffe1dd4ab01fe
SHANKAR DAKOLIA <itglobetech2020@gmail.com>
2020-10-09 12:38:18 +0530

    ...

--- src/app/customers/customers-routing.module.ts
+++ src/app/customers/customers-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CustomerListComponent } from './components/customer-list/customer-list.component';
 
 import { CustomersComponent } from './customers.component';
 
-const routes: Routes = [{ path: '', component: CustomersComponent }];
+const routes: Routes = [{ path: '', component: CustomersComponent },
+{path:'list',component:CustomerListComponent}];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/employees/employees-routing.module.ts
+++ src/app/employees/employees-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { EmployeesListComponent } from './components/employees-list/employees-list.component';
 
 import { EmployeesComponent } from './employees.component';
 
-const routes: Routes = [{ path: '', component: EmployeesComponent }];
+const routes: Routes = [
+  { path: '', component: EmployeesComponent },
+{path:'list',component:EmployeesListComponent}];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
